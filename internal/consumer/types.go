0e965e279fd938c8a29f307abcf119882c5b5667
steve-nzr <steve-theval@hotmail.fr>
2019-08-02 22:16:45 +0200

    :bug: Fixed json tag in OnDisconnect model

--- internal/consumer/types.go
+++ internal/consumer/types.go
@@ -3,7 +3,7 @@ package consumer
 // OnDisconnect structure, when back-end notifies us to disconnect a client
 type OnDisconnect struct {
 	ClientID string `json:"client_id"`
-	Reason   string `json"reason,omitempty"`
+	Reason   string `json:"reason,omitempty"`
 }
 
 // OnMessage structure, when back-end notifies us to send a message to one, multiple or all clients
